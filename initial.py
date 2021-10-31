a5c3098b232a6c78dbad6d26e7b144a56019a9c0
lawlesx <welcometohellv@gmail.com>
2021-10-31 22:12:43 +0530

    Added Reactions and Edit Message Event Handler

--- initial.py
+++ initial.py
@@ -21,5 +21,22 @@ async def on_message(message):
     if message.content == 'hello':
         await message.channel.send('Hi I am Initial')
 
+    if message.content == 'cool':
+        await message.add_reaction('\U0001F603')
+
+
+@client.event
+async def on_message_edit(before, after):
+    await before.channel.send(
+        f'{before.author} edit a message.\n'
+        f'Before: {before.content}\n'
+        f'After: {after.content}'
+    )
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    await reaction.message.channel.send(f'{user} reacted with {reaction.emoji}')
+
 
 client.run(os.getenv('DISCORD_API'))
